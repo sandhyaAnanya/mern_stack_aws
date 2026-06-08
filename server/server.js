d41e5ac20c14b3a6015b8c920e55c2cc2167e0e8
sandhyaAnanya <vsandhya5950@gmail.com>
2026-06-08 15:05:49 +0530

    added the path

--- server/server.js
+++ server/server.js
@@ -9,6 +9,14 @@ console.log("hello world");
 app.use(cors());
 app.use(express.json());
 
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
 app.get("/", (req, res) => {
   res.send("Backend Running");
 });
